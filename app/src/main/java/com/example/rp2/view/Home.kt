f6c7bf727523c566ee706486db5202d8a5d0fe1d
caioaquino <caioaquino.profissional@gmail.com>
2023-11-28 20:30:14 -0300

    refatorando tela inicial e de captura

--- app/src/main/java/com/example/rp2/view/Home.kt
+++ app/src/main/java/com/example/rp2/view/Home.kt
@@ -1,20 +1,18 @@
 package com.example.rp2.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -29,64 +27,84 @@ import com.example.rp2.ui.theme.RP2Theme
 @Composable
 fun HomeScreen(
     navController: NavController
-){
-    Column(modifier = Modifier.padding(start=16.dp,end=16.dp)){
-
-        Text(
-            text = "Bem vindo, User",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 24.dp, bottom = 24.dp),
-        )
-
-        CardImage(navController)
-
-    }
-}
-
-@Composable
-fun CardImage(
-    navController: NavController
 ) {
+    Column(modifier = Modifier.fillMaxWidth()) {
 
-    Card(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 4.dp
-        )
-    ) {
-        Column {
+        Row(Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)) {
             Image(
-                modifier = Modifier
-                    .clip(shape = RoundedCornerShape(size = 12.dp))
-                    .height(height = 196.dp),
-                painter = painterResource(id = R.drawable.img),
+                painter = painterResource(id = R.drawable.profile),
                 contentDescription = null,
-                contentScale = ContentScale.Crop,
-
-                )
-            Text(
-                modifier = Modifier.padding(start = 12.dp, top = 16.dp, bottom = 16.dp),
-                text = "Análise de imagem",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
+                modifier = Modifier.width(width = 36.dp)
             )
-
-
-            Text(
-                modifier = Modifier.padding(start = 12.dp, top = 8.dp, bottom = 16.dp),
-                text = "Converta imagens para texto, áudio, ajuste o contraste e outros!",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Normal,
-            )
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
-                Button(onClick={ navController.navigate("ImageCaptureScreen")
-                }){
-                    Text("Conferir")
-                }
-            }
 
         }
+
+
+        Row(Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)) {
+            Text(
+                text = "Bem vindo, ",
+                fontSize = 24.sp,
+            )
+            Text(
+                text = "Nicolas",
+                fontSize = 24.sp,
+                color = Color(android.graphics.Color.parseColor("#007FFF")),
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(alignment = Alignment.End)
+                .padding(top = 32.dp, start = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_3),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+            )
+        }
+
+        Text(
+            text = "Capture",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp)
+        )
+        Text(
+            text = "imagens",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 24.dp, end = 24.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "e converta seu conteúdo para texto e áudio!",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .alignByBaseline()
+                    .fillMaxWidth(0.8f)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.img_5),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(width = 56.dp)
+                    .clickable { navController.navigate("ImageCaptureScreen") }
+            )
+        }
     }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/rp2/view/ImageCapture.kt
+++ app/src/main/java/com/example/rp2/view/ImageCapture.kt
@@ -9,22 +9,27 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,22 +50,9 @@ import java.util.Objects
 @Composable
 fun ImageCaptureScreen(
     navController: NavController
-){
-    Column(
-        modifier = Modifier.padding(
-            start = 16.dp,
-            end = 16.dp,
-            bottom = 16.dp
-        )
-    ) {
+) {
+    CaptureImageContainer(navController = navController)
 
-        Text(
-            text = "Análise de imagem",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 24.dp, bottom = 24.dp),
-        )
-        CaptureImageContainer(navController)
-    }
 }
 
 fun Context.createImageFile2(): File {
@@ -83,9 +75,8 @@ fun CaptureImageContainer(
     var uri: Uri = Uri.EMPTY
     try {
         uri = FileProvider.getUriForFile(
-            Objects.requireNonNull(context.applicationContext),
-            "com.example.rp2.fileprovider", file
-        );
+            Objects.requireNonNull(context.applicationContext), "com.example.rp2.fileprovider", file
+        )
     } catch (e: Exception) {
         e.message?.let { Log.d("e", it) }
     }
@@ -95,10 +86,9 @@ fun CaptureImageContainer(
         mutableStateOf<Uri>(Uri.EMPTY)
     }
 
-    val cameraLauncher =
-        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
-            capturedImageUri = uri
-        }
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+        capturedImageUri = uri
+    }
 
     val permissionLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -117,33 +107,25 @@ fun CaptureImageContainer(
         val textRec = TextRecognizer()
         var text by remember { mutableStateOf("") }
         //context.startActivity(Intent(context, ResultsActivity::class.java))
-          val textTask: Task<Text> = textRec.getResultText(context, capturedImageUri)
-          textTask.addOnSuccessListener { visionText ->
-              text = visionText.text
-          }
-              .addOnFailureListener() { e ->
-                  e.printStackTrace()
-              }
+        val textTask: Task<Text> = textRec.getResultText(context, capturedImageUri)
+        textTask.addOnSuccessListener { visionText ->
+            text = visionText.text
+        }.addOnFailureListener() { e ->
+            e.printStackTrace()
+        }
 
         navController.navigate("ResultScreen/$text")
 
     }
 
     fun handleOnCaptureImageClick() {
-        val permissionCheckResult = (
-                ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.CAMERA
-                ) == PackageManager.PERMISSION_GRANTED &&
-                        ContextCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE
-                        ) == PackageManager.PERMISSION_GRANTED &&
-                        ContextCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.READ_EXTERNAL_STORAGE
-                        ) == PackageManager.PERMISSION_GRANTED
-                )
+        val permissionCheckResult = (ContextCompat.checkSelfPermission(
+            context, Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            context, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            context, Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
         if (permissionCheckResult) {
             cameraLauncher.launch(uri)
         } else {
@@ -154,22 +136,82 @@ fun CaptureImageContainer(
         }
 
     }
+    Column(
+        modifier = Modifier.padding(
+            start = 16.dp, end = 16.dp, bottom = 16.dp
+        )
+    ) {
 
-    Image(
-        modifier = Modifier
-            .clip(shape = RoundedCornerShape(size = 12.dp))
-            .fillMaxHeight()
-            .clickable { handleOnCaptureImageClick() },
-        painter = painterResource(id = R.drawable.img_1),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-    )
+
+        Row(Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = null,
+                modifier = Modifier.width(width = 36.dp)
+            )
+
+        }
+        Box(
+            Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)
+        ) {
+            Text(
+                text = "Capturar imagem",
+                fontSize = 24.sp,
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 80.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_6),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .align(alignment = Alignment.Center)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(top = 40.dp, start = 24.dp, end = 24.dp)
+        ) {
+            Text(
+                text = "Capture uma nova imagem ou selecione da galeria",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Button(
+            onClick = { handleOnCaptureImageClick() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(android.graphics.Color.parseColor("#4E77F8")),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp, start = 24.dp, end = 24.dp)
+        ) {
+            Text(
+                text = "Selecionar imagem",
+                color = Color.White,
+                fontSize = 20.sp,
+            )
+
+        }
+
+    }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ImageCaptureScreenPreview(){
+fun ImageCaptureScreenPreview() {
 
     val navController = rememberNavController()
 
